507ac126429ad008c1b7d91c0d1bd42bf6abb988
Filippo <69460438+FilippoO2@users.noreply.github.com>
2024-06-29 10:01:43 +0100

    Update run_trained.py

--- run_trained.py
+++ run_trained.py
@@ -7,7 +7,7 @@ generator=torch.Generator().manual_seed(seed_value)
 
 pipe_id = "stabilityai/stable-diffusion-3-medium-diffusers"
 pipe = DiffusionPipeline.from_pretrained(pipe_id, torch_dtype=torch.float16).to("cuda")
-pipe.load_lora_weights("/path/to/trained/model_dir", weight_name="model.safetensors",adapter_name="trained")
+pipe.load_lora_weights("~/fine_tuned_model", weight_name="pytorch_lora_weights.safetensors",adapter_name="trained")
 pipe.fuse_lora(adapter_names=["trained"], lora_scale=0.9)
 
 prompt = "PROMPT HERE"
@@ -22,4 +22,4 @@ image = pipe(
     guidance_scale=7.0,
     generator=generator,
 ).images[0]
-image.save("lora_newest.png")
+image.save("output.png")
